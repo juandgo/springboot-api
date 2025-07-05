a1b72049ba596cc488a48378f3a38a48e9756c56
Juan David Grijalba Osorio <55849409+juandgo@users.noreply.github.com>
2025-07-05 08:59:51 -0500

    h 3:30

--- curso-introduccion-apis/src/main/java/com/lta/cursoapis/curso_introduccion_apis/controller/ProductoController.java
+++ curso-introduccion-apis/src/main/java/com/lta/cursoapis/curso_introduccion_apis/controller/ProductoController.java
@@ -1,5 +1,6 @@
 package com.lta.cursoapis.curso_introduccion_apis.controller;
 
+import com.lta.cursoapis.curso_introduccion_apis.entity.Categoria;
 import com.lta.cursoapis.curso_introduccion_apis.entity.EstadoProducto;
 import com.lta.cursoapis.curso_introduccion_apis.entity.Producto;
 import com.lta.cursoapis.curso_introduccion_apis.service.ProductoService;
@@ -19,66 +20,97 @@ public class ProductoController {
     @Autowired
     private ProductoService productoService;
 
-    @PostMapping("/registrar")
-    public ResponseEntity<?> registrarProducto(@RequestBody Producto producto){
-        Producto nuevoProducto = productoService.registrarProducto(producto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(nuevoProducto);
+    @PostMapping("/registrar/{categoriaId}")
+    public ResponseEntity<?> registrarProducto(
+            // @RequestBody Producto producto
+            @PathVariable(required = true) Long categoriaId,
+            @RequestParam("nombreProducto") String nombreProducto,
+            @RequestParam("descripcion") String descripcion,
+            @RequestParam("precio") Double precio,
+            @RequestParam("cantidad") int cantidad,
+            @RequestParam("estado") EstadoProducto estado) {
+        Producto producto = new Producto();
+        producto.setNombreProducto(nombreProducto);
+        producto.setDescripcion(descripcion);
+        producto.setPrecio(precio);
+        producto.setCantidad(cantidad);
+        producto.setEstadoProducto(estado);
+
+        Producto productoBBDD = productoService.registrarProducto(producto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(producto);
     }
 
     @GetMapping
-    public ResponseEntity<List<Producto>> listarProductos(){
+    public ResponseEntity<List<Producto>> listarProductos() {
         List<Producto> productos = productoService.listarProductos();
         return ResponseEntity.ok(productos);
     }
 
     @GetMapping("/buscar/nombre/{nombre}")
-    public ResponseEntity<?> buscarPorNombre(@PathVariable String nombre){
+    public ResponseEntity<?> buscarPorNombre(@PathVariable String nombre) {
         Optional<Producto> producto = productoService.buscarPorNombre(nombre);
         return producto.isPresent() ? ResponseEntity.ok(producto.get())
                 : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Producto no encontrado");
     }
 
     @GetMapping("/buscar/id/{idProducto}")
-    public ResponseEntity<?> buscarPorId(@PathVariable Long idProducto){
+    public ResponseEntity<?> buscarPorId(@PathVariable Long idProducto) {
         Optional<Producto> producto = productoService.buscarPorId(idProducto);
         return producto.isPresent() ? ResponseEntity.ok(producto.get())
                 : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Producto no encontrado");
     }
 
     @PutMapping("/actualizar/{idProducto}")
-    public ResponseEntity<?> actualizarProducto(@PathVariable Long idProducto,@RequestBody Producto producto){
-        try{
-            Producto productoActualizado = new Producto();
-            productoActualizado.setIdProducto(idProducto);
-            productoActualizado.setNombreProducto(producto.getNombreProducto());
-            productoActualizado.setPrecio(producto.getPrecio());
-            productoActualizado.setDescripcion(producto.getDescripcion());
-            productoActualizado.setCantidad(producto.getCantidad());
-            productoActualizado.setEstadoProducto(producto.getEstadoProducto());
+    public ResponseEntity<?> actualizarProducto(
+            // @PathVariable Long idProducto,@RequestBody Producto producto
+            @PathVariable Long idProducto,
+            @RequestParam("nombreProducto") String nombreProducto,
+            @RequestParam("descripcion") String descripcion,
+            @RequestParam("precio") Double precio,
+            @RequestParam("cantidad") int cantidad,
+            @RequestParam("estado") EstadoProducto estado) {
+        try {
+//            Producto productoActualizado = new Producto();
+            // productoActualizado.setIdProducto(idProducto);
+            // productoActualizado.setNombreProducto(producto.getNombreProducto());
+            // productoActualizado.setPrecio(producto.getPrecio());
+            // productoActualizado.setDescripcion(producto.getDescripcion());
+            // productoActualizado.setCantidad(producto.getCantidad());
+            // productoActualizado.setEstadoProducto(producto.getEstadoProducto());
+            Producto producto = new Producto();
+            producto.setNombreProducto(nombreProducto);
+            producto.setDescripcion(descripcion);
+            producto.setPrecio(precio);
+            producto.setCantidad(cantidad);
+            producto.setEstadoProducto(estado);
 
-            Producto productoBBDD = productoService.actualizarProducto(idProducto, productoActualizado);
+//            Producto productoBBDD = productoService.actualizarProducto(idProducto, productoActualizado);
+            Producto productoActualizado = productoService.actualizarProducto(idProducto, producto);
             return ResponseEntity.ok(productoActualizado);
-        }catch (Exception exception){
+        } catch (Exception exception) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
-//            System.out.println(exception.getMessage());
+            // System.out.println(exception.getMessage());
         }
     }
 
     @DeleteMapping("/{idProducto}")
-    public ResponseEntity<?> eliminarProducto(@PathVariable Long idProducto){
+    public ResponseEntity<?> eliminarProducto(@PathVariable Long idProducto) {
         try {
             productoService.eliminarProducto(idProducto);
-//            return ResponseEntity.status(HttpStatus.NO_CONTENT).body();
+            // return ResponseEntity.status(HttpStatus.NO_CONTENT).body();
             return ResponseEntity.noContent().build();
-        }catch (Exception exception){
-//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+        } catch (Exception exception) {
+            // return
+            // ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build(); // 404 sin cuerpo
         }
     }
 
     @PutMapping("/estado/{idProducto}")
-    public ResponseEntity<?> cambiarEstadoProducto(@PathVariable Long idProducto, @RequestBody EstadoProducto estadoProducto){
-        try{
+    public ResponseEntity<?> cambiarEstadoProducto(@PathVariable Long idProducto,
+            @RequestBody EstadoProducto estadoProducto) {
+        try {
             Producto productoActualizado = productoService.cambiarEstadoProducto(idProducto, estadoProducto);
             return ResponseEntity.ok(productoActualizado);
         } catch (Exception e) {
@@ -87,8 +119,8 @@ public class ProductoController {
     }
 
     @GetMapping("/estado/{estado}")
-    public ResponseEntity<List<Producto>> listarProductosPorEstado(@PathVariable EstadoProducto estado){
+    public ResponseEntity<List<Producto>> listarProductosPorEstado(@PathVariable EstadoProducto estado) {
         List<Producto> productos = productoService.obtenerProductosPorEstado(estado);
-        return  ResponseEntity.ok(productos);
+        return ResponseEntity.ok(productos);
     }
 }
